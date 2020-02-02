718ac9c36be47d0b42dbfd90e2335e86d5121d46
YukiFujimine <study.yuki0115.gmail.com>
2020-02-02 16:09:54 +0900

    jtjavaQ_d6Q1

---
+++ day6/d6Q1.java
@@ -0,0 +1,12 @@
+public class d6Q1{
+	public static void main(String[] args){
+			int[] arr;
+			arr=new int[3];
+			arr[0]=30;
+			arr[1]=70;
+			arr[2]=10;
+			for(int i=0;i<arr.length;i++){
+			System.out.println("arr["+i+"]:"+arr[i]);
+			}
+	}
+}
