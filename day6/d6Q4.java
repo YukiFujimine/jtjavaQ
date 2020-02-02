f88418b8233df11b1cef630f7c365fd338840432
YukiFujimine <study.yuki0115.gmail.com>
2020-02-02 16:11:32 +0900

    jtjavaQ_d6Q4

---
+++ day6/d6Q4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class d6Q4{
+	public static void main(String[] args){
+			char[] greeting={'H','E','L','L','O','!'};
+			for(int i=0;i<greeting.length;i++){
+			System.out.print(greeting[i]);
+			}
+	}
+}
