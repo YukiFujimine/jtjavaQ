3949b4fcd9f9956537e99f32f38eb8217ac3c31a
YukiFujimine <study.yuki0115.gmail.com>
2020-02-02 16:10:15 +0900

    jtjavaQ_d6Q2

---
+++ day6/d6Q2.java
@@ -0,0 +1,12 @@
+public class d6Q2{
+	public static void main(String[] args){
+			String[] fruit;
+			fruit=new String[3];
+			fruit[0]="Banana";
+			fruit[1]="Apple";
+			fruit[2]="Orange";
+			for(int i=0;i<fruit.length;i++){
+			System.out.println("fruit["+i+"]:"+fruit[i]);
+			}
+	}
+}
